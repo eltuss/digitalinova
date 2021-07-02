0a992614c3235d1b7158e36704f2e4e1a6202500
Eltuss <elton.s.barbosa@outlook.com>
2021-07-02 11:51:10 -0300

    Cirado Cliente Enum com tipo forEach

---
+++ src/main/ClienteTipo.kt
@@ -0,0 +1,6 @@
+package main
+
+enum class ClienteTipo(val descricao: String) {
+    PF("Pessoa Fisica"),
+    PJ("Pessoa Juridica")
+}
---
+++ src/main/teste/TesteClienteTipoEnum.kt
@@ -0,0 +1,16 @@
+package main.teste
+import main.ClienteTipo
+
+fun main(){
+    ClienteTipo.values().forEach { elemento ->
+        println("${elemento.name} - ${elemento.descricao}")
+    }
+    ClienteTipo.values().forEach {
+        println("${it.name} - ${it.descricao}")
+    }
+    val pf = ClienteTipo.PF
+    println(">> ${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println(">> ${pj.name} - ${pj.descricao}")
+}
