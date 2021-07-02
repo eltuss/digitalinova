b974822f3887a5fda2135da6fa3af8d8ec44c957
Eltuss <elton.s.barbosa@outlook.com>
2021-07-01 22:46:30 -0300

    Initial commit

---
+++ src/main/Pessoa.kt
@@ -0,0 +1,15 @@
+package main
+
+class Pessoa{
+    var nome: String = "Elton"
+    var cpf: String = "123.123.123-01"
+
+
+}
+fun main() {
+    val elton = Pessoa()
+
+    println(elton.nome)
+    println(elton.cpf)
+
+}
