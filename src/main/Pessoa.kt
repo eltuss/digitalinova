8f2849c47d78f29427513aff2514f51c065a24d7
Eltuss <elton.s.barbosa@outlook.com>
2021-07-01 23:13:26 -0300

    atualização sobre os membros de uma classe

--- src/main/Pessoa.kt
+++ src/main/Pessoa.kt
@@ -4,11 +4,14 @@ class Pessoa{
     var nome: String = "Elton"
     var cpf: String = "123.123.123-01"
     private set
+
+    //Interpolação
+    fun pessoaInfo() = "$nome e $cpf"
+
 }
 fun main() {
     val elton = Pessoa()
 
-    println(elton.nome)
-    println(elton.cpf)
+    println(elton.pessoaInfo())
 
 }
